442b17d940927a5e0fb19e3cd56f29c5126bd85b
Erik O'Hara <slicedblue@gmail.com>
2020-10-13 21:13:26 -0400

    optimized iterative_translator.py a bit, had to change name of googletrans.py to stop local module from loading

--- iterative_translator.py
+++ iterative_translator.py
@@ -1,38 +1,35 @@
-import googletrans
-import csv
-import httpx
+import googletrans, sys, csv, httpx, datetime, time
 
 # Replace [PATH] Strings appropriately for your machine.
 # For the two temporary files, I named them after the file being translated (i.e. for 'bags' they'd be 'bags_tran1.csv' and 'bags_tran2.csv')
 # This only translates one file at a time; you can obviously alter it to do all of them but it'll take forever.
 # I've been running a couple/bunch simultaneously in different terminals
 
-is_translated = False
-fn1 = '[PATH TO TEMPORARY FILE 1]'
-fn2 = '[PATH TO TEMPORARY FILE 2]'
+# if there's a sysarg for a file name, use that, otherwise use hard-coded value
+if len(sys.argv) <= 1:
+    print("Please launch the program in the command line! (ex. \"python iterative_translator.py <csv filename to translate>\"")
+    print("Press enter to exit.")
+    input()
+    sys.exit(0)
+else:
+    fileToTranslate = sys.argv[1]
 
+# flags and hard-coded paths as well
+is_translated = False
+fn1 = fileToTranslate[:-4] + "_translated.csv"
+
+# create a translator and a timeout for it
 timeout = httpx.Timeout(5)
 translator = googletrans.Translator(timeout=timeout)
+start_time = datetime.datetime.now()
 
-print('Beginning translation of [file] at [time]...')
+print('Initialization complete!')
+print(f'Beginning translation of {fileToTranslate} at {start_time.strftime("%H:%M:%S")}...')
 
-# Initial translation
-with open('[PATH TO INITIAL, UNTRANSLATED FILE]', encoding='utf-8') as initial:
-    with open(fn1, 'w+', encoding='utf-8', newline='') as t1:
-        reader = csv.reader(initial)
-        writer = csv.writer(t1)
-        for line in reader:
-            lc = line.copy()
-            if translator.detect(lc[1]).lang == 'fr': 
-                lc[1] = translator.translate(lc[1], src='fr', dest='en').text
-            if translator.detect(lc[2]).lang == 'fr':
-                lc[2] = translator.translate(lc[2], src='fr', dest='en').text
-            writer.writerow(lc)
-
-# Forced back-and-forth translation until none of the product names are in French
+# now translate until none detected
 while not is_translated:
-    with open(fn1, encoding='utf-8') as f1:
-        with open(fn2, 'w+', encoding='utf-8', newline='') as f2:
+    with open(fileToTranslate, 'r', encoding='utf-8') as f1:
+        with open(fn1, 'w+', encoding='utf-8', newline='') as f2:
             reader = csv.reader(f1)
             writer = csv.writer(f2)
             is_translated = True
@@ -40,6 +37,9 @@ while not is_translated:
             for line in reader:
                 try:
                     lc = line.copy()
+                    if translator.detect(lc[1]).lang == 'fr':
+                        is_translated = False
+                        lc[1] = translator.translate(lc[1], src='fr', dest='en').text
                     if translator.detect(lc[2]).lang == 'fr':
                         is_translated = False
                         lc[2] = translator.translate(lc[2], src='fr', dest='en').text
@@ -48,6 +48,12 @@ while not is_translated:
                 
             # Switches the files
             fholder = f1
-            f1 = f2; f2 = fholder
+            f1 = f2
+            f2 = fholder
 
-print(f1,'should be fully translated.')
+end_time = datetime.datetime.now()
+print(f"[SUCCESS IN {datetime.datetime.now() - start_time} SECONDS]")
+print(f"{fileToTranslate} has been translated and saved into filename \"{fn1}\".")
+print("Press enter to exit.")
+input()
+sys.exit(0)
