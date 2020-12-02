54e432efee88670f139b9ea8a625313776ed6bce
P Atrey <patrey@Ps-MacBook-Pro.local>
2020-12-02 00:21:41 -0500

    added final files for project

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ As an example, training a Naïve Bayes-based classifier on data in a file data_f
 train(data_file, "nb");
 ```
 
-The *method* parameter dictates the type of classifier that will be trained on the data.
+The *method* parameter dictates the type of classifier that will be trained on the data; see **Which Classifier Should I Use?** for help picking a method, as well as train’s documentation for more information.
 
 Including the *test* parameter allows the user to set aside a certain percentage of their data as ‘test data.’ This test data will be randomly selected from the dataset and will not contribute to the classifier’s training. Once the classifier is trained on the non-test data, it will try to classify the test data based on what it has learned. The program will then compare the classifier’s guess with the actual value for each test element and print a numerical representation of its accuracy. Bear in mind that smaller test data pools may report lower-than-actual accuracy, and larger pools mean less actual training data for the classifier. What constitutes a good value depends on the overall data size, but generally ~10-15% should yield a fair representation of accuracy. 
 
@@ -36,18 +36,23 @@ For best results, any files passed to train should adhere to the following guide
 - The file should be a .csv file in English, with the delimiter set as “,” and the encoding set as “utf-8”.
 - The first line of each file should delineate the headers for the products contained therein. For example, a header line might look like this:
 ```python
-category, subcategory, name, ...
+category, name, color, price
 ```
-- The first header should represent the desired primary category, followed by a subcategory. Every product should have a meaningful, non-null – value for this attribute (otherwise the product can’t really contribute anything to the training process).
+- The first header should represent the desired primary determinant (i.e. whatever you’re trying to classify items into). Every product should have a meaningful, non-null – value for this attribute (otherwise the product can’t really contribute anything to the training process).
+  - In most cases the primary determinant will be a product’s category: the classifier might be asked to place an item ‘lawn chair’ into one of a number of categories, for example.
 - Every product in the file should have a value for every header. This means that even if a product doesn’t have a known value for an attribute that attribute should still be represented by an empty string or some other null value.
   - To follow the earlier example, a product without a known color would be listed as:
   ```python
-  toys, stuffed animals, “teddy bear”,, “$5”
+  toys, “teddy bear”,, “$5”
   ```
+- Attributes with units should have that unit included in their value.
+  - i.e. “toys, ‘teddy bear’,, ‘$5’” rather than “toys, ‘teddy bear’,, 5” with the header being “price ($)”
+  
+When passing multiple files to train it’s important that the primary determinant for every file is the same; if the classifier is supposed to be recognizing/associating product categories for one dataset and prices for another it’s ultimately not likely to perform very well for either. 
 
 ## Method List
 
- - train(files, ”classifier_method_key”, test=%_test_data, svm_kernel=%_kernel_type, noise_len=%_str_len)
+ - train(files, ”classifier_method_key”, test=%_test_data)
     - Parameters
       - files: The file(s) that the classifier will be trained on. Can be a single file or an array of files.
       - method: String: The specific classifier method that will be used. Potential values are as follows:
@@ -55,13 +60,9 @@ category, subcategory, name, ...
         - lr: Logistic Regression
         - tree: Tree Classifier
         - svm: SVM
-      - test: 
-        - int : Pulls aside a percentage of the data in files to use as test data. Prints a measure of accuracy for the trained classifier
-        - String : Attempts to use the String as a file name, and pulls indices from that file that represent data elements to withhold for testing.
-      - kernel_type: String: Defines the kernel type to be used for the SVM classification method.
-      - noise_len: int: The length of a randomly-generated String that will be appended to the end of a product's name (used to test the classifier's robustness).
+      - test: int : Pulls aside a percentage of the data in files to use as test data. Prints a measure of accuracy for the trained classifier. 
     - Returns
-      - A dictionary containing classifier objects, trained on files using the specified method (also includes the accuracy and training time for the classifier if the 'test' variable was used).
+      - A classifier object, trained on files using the specified method.
   - classify(items, classifier=[classifier])
     - Parameters
       - items: The String(s) that the classifier will attempt to classify. Can be a single String or an array of Strings.
--- code/classi.py
+++ code/classi.py
@@ -1,49 +1,72 @@
+# classi.py - Team 4
+# Member Contributions:
+# Jordan Le: 25%
+# Pranjal Atrey: 25%
+# Alex Kisiel: 25%
+# Erik O'Hara: 25%
+
+# imports for our used libraries
 import csv
-import sklearn
-import pandas as pd
 import numpy as np
+import tkinter as tk
+import random as rand
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics, svm
-import random as rand
-import string
-import time
 
-# Generates a String of random characters of length 'wordlen'
-def gen_rand_word(wordlen):
-    word = ''
-    for i in range(wordlen):
-        word += rand.choice(string.ascii_letters)
-    return word
+# This function is used to get the top n words from our product names
+def get_top_n_words(corpus, n=None):
+    vec = CountVectorizer().fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
 
-def concat_files(files):
-    return
+# method to train a classifier on a data set
+def train():
 
-def train(f, method, *args, **kwargs):
     # Holds whatever it is that the classifier will differentiate on
     cats = []
-    subcats = []
     testcats = []
     # Holds the values that will be tokenized for the bag of words
     values = []
     testvalues = []
+    subcats = []
 
-    if isinstance(f, list):
-        file = concat_files(f)
-    else:
-        file = f
-    test = kwargs.get('test', None)
+    # gets the file name from the UI
+    try:
+        file = file_name.get()
+    except:
+        print(f"File name {file_name.get()} not found. Try again.")
+        return
+    
+    # gets the method selection from the UI
+    try:
+        method = int(method_name.get())
+        if method == 1:
+            method = "nb"
+        elif method == 2:
+            method = "lr"
+        elif method == 3:
+            method = "tree"
+        elif method == 4:
+            method = "svm"
+    except:
+        print("Please select a valid classifier method and try again.")
+        return
+    
+    # gets the withheld testing data percentage from the UI
+    try:
+        test = int(test_name.get())
+    except:
+        print("Test value not loaded. Defaulting to 20%.")
+        test = 20
 
-    # Sets the SVM kernel (if applicable)
-    svm_kernel = kwargs.get('svm_kernel', 'linear')
-    # Sets the length of the 'noise' words
-    noise_len = kwargs.get('noise_len', 0)
-
-    cw = {}
-
+    # Opens the CSV file to read every line
     with open(file, encoding='utf-8') as f:
         reader = csv.reader(f)
         header = reader.__next__()
@@ -60,11 +83,17 @@ def train(f, method, *args, **kwargs):
                     values[len(values)-1] += ' ' + line[n]
             except Exception as e:
                 True 
-            values[len(values)-1] += ' ' + gen_rand_word(noise_len)
-              
 
         # Replace 'cats' here if you want to train on subcats
         train_on_this = cats.copy()
+
+
+        # print("-------------------")
+        # print("Amazon Dataset")
+        # print("-------------------")
+        # common_words = get_top_n_words(values, 10)
+        # for word, freq in common_words:
+        #     print(word, freq)
 
         if test:
             # If a String is passed to test it's assumed that it is a filename containing a number of indices
@@ -79,35 +108,29 @@ def train(f, method, *args, **kwargs):
                 print(len(testcats), 'data items withheld for testing...')
             elif isinstance(test, int):
                 # Stratification: Randomly selects a percentage of elements for training from *each category* (rather than a blanket percentage over the whole dataset)
-                # Commented out code is for creating csv files with indices
+                start = 0
+                indices = []
+                currentcat = train_on_this[0]
+                for i in range(len(train_on_this)):
+                    if train_on_this[i] == currentcat:
+                        True
+                    else:
+                        # Get the test data for the curent category and add it to the test data indices
+                        for r in rand.sample(range(start, i), int((i-start) * (test/100))):
+                            indices.append(r)
+                        currentcat = train_on_this[i+1]
+                        start = i
+                # Get the test percentage for the final category
+                ran = rand.sample(range(start, i), int((i-start) * (test/100)))
+                for r in ran:
+                    indices.append(r)
 
-                with open('testgamut/testdata' + str(test) + '.csv', 'w+', encoding='utf-8', newline='') as testfile:
-                    writer = csv.writer(testfile)
+                indices = sorted(indices, reverse=True)
+                for i in indices:
+                    testcats.append(train_on_this.pop(i))
+                    testvalues.append(values.pop(i))
 
-                    start = 0
-                    indices = []
-                    currentcat = train_on_this[0]
-                    for i in range(len(train_on_this)):
-                        if train_on_this[i] == currentcat:
-                            True
-                        else:
-                            # Get the test data for the curent category and add it to the test data indices
-                            for r in rand.sample(range(start, i), int((i-start) * (test/100))):
-                                indices.append(r)
-                            currentcat = train_on_this[i+1]
-                            start = i
-                    # Get the test percentage for the final category
-                    ran = rand.sample(range(start, i), int((i-start) * (test/100)))
-                    for r in ran:
-                        indices.append(r)
-
-                    indices = sorted(indices, reverse=True)
-                    for i in indices:
-                        writer.writerow([i])
-                        testcats.append(train_on_this.pop(i))
-                        testvalues.append(values.pop(i))
-
-                print(len(testcats),'data items withheld for testing...')
+                print(f"{len(testcats)} data items withheld for testing...")
         
         count_vect = CountVectorizer()
         # This is where the bag of words is created:
@@ -118,32 +141,26 @@ def train(f, method, *args, **kwargs):
         tf_transformer = TfidfTransformer()
         train_tf = tf_transformer.fit_transform(vectorized)
 
+        # selects Naive Bayes as classifier method
         if method == 'nb':
             # Train the classifier
-            train_start = time.time()
             cw['classifier'] = MultinomialNB().fit(train_tf, train_on_this)
-            cw['train_time'] = time.time() - train_start
 
+        # selects Logistic Regression as classifier method
         elif method == 'lr':
-            train_start = time.time()
             cw['classifier'] = LogisticRegression(max_iter = 10000, class_weight='balanced').fit(train_tf, train_on_this)
-            cw['train_time'] = time.time() - train_start
 
+        # selects Decision Tree as classifier method
         elif method == 'tree':
-	        # Create Decision Tree classifer object
+            # Create Decision Tree classifer object
             cw['classifier'] = DecisionTreeClassifier(class_weight='balanced')
-	        # Train Decision Tree Classifer
-            train_start = time.time()
+            # Train Decision Tree Classifer
             cw['classifier'] = cw['classifier'].fit(train_tf, train_on_this)
-            cw['train_time'] = time.time() - train_start
 
+        # selects SVM as classifier method
         elif method == 'svm':
-
-            # starts running the SVM classifier
-            clf = svm.SVC(kernel = svm_kernel, class_weight='balanced') # Linear Kernel
-            train_start = time.time()
+            clf = svm.SVC(kernel = 'linear', class_weight='balanced') # Linear Kernel
             cw['classifier'] = clf.fit(train_tf, train_on_this)
-            cw['train_time'] = time.time() - train_start
 
         else:
             print('Must enter an acceptable method')
@@ -157,33 +174,114 @@ def train(f, method, *args, **kwargs):
             newcounts = count_vect.transform(testvalues)
             new_tfidf = tf_transformer.transform(newcounts)
             #Predict the test data's category
-            pstart = time.time()
             predicted = cw['classifier'].predict(new_tfidf)
-            cw['predict_time'] = time.time() - pstart
             # Set classifier accuracy
             acc = int(np.mean(predicted == testcats)*100)
+            print(f"Success! Your classifier has a prediction accuracy of {acc}%. You can now classify items with this classifier using the panel on the right side of the user interface, or re-train a new classifier.")
             cw['acc'] = acc
-
-            # Writes results to a file in case you want to see what the actual predictions look like
-            # with open('testfile.csv', 'w+', encoding='utf-8', newline='') as f:
-            #     writer = csv.writer(f)
-            #     for doc, category in zip(testvalues, predicted):
-            #         writer.writerow([doc, ':', category])
     
-
     return cw
 
-def classify(classifand, classifier):
-    if not isinstance(classifand, list):
+
+# classifier method, runs when user inputs product on righthand side
+def classify():
+    classifier = cw
+
+    # gets product name from UI
+    try:
+        classifand = product_name.get()
         c = [classifand]
-    else:
-        c = classifand
-    #Fit the String to be classified
+    except:
+        print("You must enter a valid product name. Please try again.")
+        return
+
+    # adds color from UI if applicable
+    try:
+        c[0] = c[0] + " " + str(color_name.get())
+    except:
+        pass
+
+    # adds price from UI if applicable
+    try:
+        c[0] = c[0] + " " + str(price_name.get())
+    except:
+        pass
+
+    # Fit the String to be classified
     v = classifier['count_vect'].transform(c)
     n = classifier['tft'].transform(v)
+
     # Actual prediction step
     predicted = classifier['classifier'].predict(n)
-    i = 0
-    for p in predicted:
-        print(classifand[i], ':', p)
-        i += 1
+    print("------------------------------")
+    print("Prediction Complete!")
+    print(f"Your product {classifand} belongs in the {predicted[0]} category!")
+    return
+
+
+# Holds whatever it is that the classifier will differentiate on
+cw = {}
+cats = []
+subcats = []
+testcats = []
+        
+# Holds the values that will be tokenized for the bag of words
+values = []
+testvalues = []
+        
+# create the UI
+# build a blank interface
+interface = tk.Tk()
+interface.geometry("450x550")
+
+# create variables that will be used to grab data from UI
+file_name = tk.StringVar()
+product_name = tk.StringVar()
+test_name = tk.StringVar()
+method_name = tk.IntVar()
+color_name = tk.StringVar()
+price_name = tk.StringVar()
+
+# create name at top
+label_name = tk.Label(interface, text = "Classi.py, a Simple Classifier", font = ("Arial", 24)).grid(ipady = 5, row = 0, column = 1, columnspan = 2)
+interface.title("Classi.py")
+
+# create two separate columns for different data entry
+label_testing = tk.Label(interface, text = "Training Entry", font = ("Arial", 16)).grid(pady = (10, 0), padx = (5, 0), row = 1, column = 1)
+label_data = tk.Label(interface, text = "Classify Entry", font = ("Arial", 16)).grid(pady = (10, 0), row = 1, column = 2)
+
+# TESTING (LEFT) COLUMN DATA
+# create row for entering the file name
+label_file = tk.Label(interface, text = "CSV Data Set Path", font = ("Arial", 16)).grid(pady = (10, 0), padx = (15, 0), row = 3, column = 1)
+entry_file = tk.Entry(interface, width = 30, textvariable = file_name).grid(ipady = 5, row = 4, column = 1)
+
+# create row for entering the test percentage
+label_test = tk.Label(interface, text = "Testing % Withheld", font = ("Arial", 16)).grid(pady = (10, 0), row = 5, column = 1)
+entry_test = tk.Entry(interface, width = 30, textvariable = test_name).grid(ipady = 5, row = 6, column = 1)
+
+# create the radio button options for the classifier methods
+label_method = tk.Label(interface, text = "Classifier Method", font = ("Arial", 16)).grid(pady = (25, 0), row = 7, column = 1)
+radio_nb = tk.Radiobutton(interface, text = "Naive Bayes", variable = method_name, value = 1).grid(pady = (5, 0), row = 8, column = 1)
+radio_lr = tk.Radiobutton(interface, text = "Logistic Regression", variable = method_name, value = 2).grid(pady = (5, 0), row = 9, column = 1)
+radio_tree = tk.Radiobutton(interface, text = "Tree", variable = method_name, value = 3).grid(pady = (5, 0), row = 10, column = 1)
+radio_swm = tk.Radiobutton(interface, text = "SWM", variable = method_name, value = 4).grid(pady = (5, 0), row = 11, column = 1)
+
+button_train = tk.Button(interface, text = "Train Classifier", command = train, width = 15, height = 2).grid(pady = (50, 0), row = 12, column = 1)
+
+# CLASSIFYING (RIGHT) COLUMN DATA
+# create row for entering a product name
+label_product = tk.Label(interface, text = "Product Description", font = ("Arial", 16)).grid(pady = (10, 0), padx = 10, row = 3, column = 2)
+entry_product = tk.Entry(interface, width = 30, textvariable = product_name).grid(ipady = 5, row = 4, column = 2)
+
+label_color = tk.Label(interface, text = "Product Color", font = ("Arial", 16)).grid(pady = (10, 0), row = 5, column = 2)
+entry_color = tk.Entry(interface, width = 30, textvariable = color_name).grid(ipady = 5, row = 6, column = 2)
+
+label_price = tk.Label(interface, text = "Product Price", font = ("Arial", 16)).grid(pady = (10, 0), row = 7, column = 2)
+entry_price = tk.Entry(interface, width = 30, textvariable = price_name).grid(ipady = 5, row = 8, column = 2)
+
+# allow the user to submit the data to the program
+button_classify = tk.Button(interface, text = "Classify Product", command = classify, width = 15, height = 2).grid(pady = (50, 0), row = 12, column = 2)
+
+
+# run the build infinitely until user closes out
+interface.mainloop()
